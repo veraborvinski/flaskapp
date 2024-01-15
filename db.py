96e63ebfcc1995fad336fd9639d97ef8af273f2b
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2024-01-15 01:03:04 +0000

    Update db.py

--- db.py
+++ db.py
@@ -49,7 +49,7 @@ def create_database(cnx,cursor,DB_NAME):
     cursor.close()
     cnx.close()
 
-def insert_title(cnx,cursor,title,watchtime,DB_NAME):
+def insert_title(cnx,cursor,title,DB_NAME):
     update_statement = ""
     if check_if_item_on_watchlist(cnx,cursor,title,DB_NAME):
         update_statement = "UPDATE "+DB_NAME+"+.WatchList SET watchtime='0:00' WHERE title LIKE %'"title"'%;"
